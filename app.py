5dbf0628e315f2f0f36d21b77a79c86c3a4aeec6
900224s-alt <900224s@gmail.com>
2026-06-18 14:25:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,10 +49,10 @@ if 'logged_in' not in st.session_state: st.session_state['logged_in'] = False
 st.title("📦 物流退貨點收系統")
 
 if not st.session_state['logged_in']:
-    tab1, tab2 = st.tabs(["👤 帳號登入", "📝 新人員註冊"])
-    with tab1:
-        login_name = st.text_input("余宸緯", key="login_name").strip()
-        login_pwd = st.text_input("0224", type="password", key="login_pwd")
+    tab1, tab2 = st.tabs(["👤 帳號登入", "📝 新用戶註冊"])
+   with tab1:
+        login_name = st.text_input("請輸入中文真實姓名", key="login_name").strip()
+        login_pwd = st.text_input("請輸入密碼", type="password", key="login_pwd")
         if st.button("進入系統"):
             conn = get_db_connection()
             user = conn.execute('SELECT * FROM users WHERE username = ?', (login_name,)).fetchone()
