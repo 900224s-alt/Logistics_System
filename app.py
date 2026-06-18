56c490b49a700c82c07334664b36fd8c1e38ef83
900224s-alt <900224s@gmail.com>
2026-06-18 11:08:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def get_tw_now():
   conn.commit(); st.success("註冊成功！") 
  except: st.error("❌ 姓名已被註冊。") 
  finally: conn.close() 
- else:
+else:
     st.sidebar.write(f"👤 作業員：**{st.session_state['username']}**")
     st.sidebar.write(f"🎖️ 權限：**{'管理者' if st.session_state.get('is_admin') else '一般用戶'}**")
 
