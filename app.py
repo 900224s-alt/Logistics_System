191ed688f9bf31bc49e8461e147d5c9763726168
900224s-alt <900224s@gmail.com>
2026-06-22 12:30:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ if 'logged_in' not in st.session_state:
     else:
         st.session_state['logged_in'] = False
 
-st.title("📦 物流退貨點收系統 (極速雲端版)")
+st.title("📦 特捷物流退貨點收系統 ")
 
 if not st.session_state['logged_in']:
     tab1, tab2 = st.tabs(["👤 帳號登入", "📝 新用戶註冊"])
